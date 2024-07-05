a008543cec713c594e71db0e11a710264b067ebe
iAmAy0mide <ayomidekenny952@gmail.com>
2024-07-05 02:05:28 -0700

    update token exp back to 15m

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const generateAccessToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' }); 
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '15m' }); 
 };
 
 const generateRefreshToken = (id) => {
